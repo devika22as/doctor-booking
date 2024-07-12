7d987d4052aed3ee33bbf689f68f42401cf886e3
devika22as <asdevika056@gmail.com>
2024-07-12 11:06:16 +0530

    Adding initial folders

---
+++ server/routes/index.js
@@ -0,0 +1,6 @@
+const express=require('express')
+const departmentRoutes=require('./department-routes')
+const router=express.Router()
+router.use('/department',departmentRoutes)
+
+module.exports=router
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app=express()
+const cors =require('cors')
+const dotenv=require('dotenv')
+dotenv.config({path:'./env'})
+const db=require('./Db')
+ app.use(cors())
+ app.use(express.json())
+ app.use(express.static('public'))
+
+ const routes=require('./routes')
+ app.use(routes)
+
+ app.listen(process.env.PORT,()=>{
+    console.log('App is running...')
+    console.log('http://localhost:8000/')
+ })
